d7a2b1b35244efeab1dc3dd1e1897b948d5ab703
juanbocanegraformacion-prog <juanbocanegraformacion@gmail.com>
2026-04-21 17:09:42 -0400

    Update URL for fetching data in calendar_app.py

--- calendar_app.py
+++ calendar_app.py
@@ -178,7 +178,8 @@ else:
         return pd.read_excel(io.BytesIO(requests.get(url).content), engine='openpyxl')
 
     
-   url = "https://raw.githubusercontent.com/juanbocanegraformacion-prog/Calendario_Proveedor/main/ODC_CENDI_GUATIRE_20260421_1110.xlsx"
+   url = "https://raw.githubusercontent.com/juanbocanegraformacion-prog/Calendario_Proveedor/main/%C3%93rdenes%20de%20compra%2016_04_2026.xlsx"
+  # url = "https://raw.githubusercontent.com/juanbocanegraformacion-prog/Calendario_Proveedor/main/ODC_CENDI_GUATIRE_20260421_1110.xlsx"
 
     
     try:
